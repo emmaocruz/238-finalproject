51edb07dd5bee6dbdb32cd39b36d3eb1d2163e2d
Emma Cruz <emmacruz@stanford.edu>
2024-12-04 23:11:11 -0800

    update interface

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ from pitch_perfect import PitchPerfect
 
 def run_notebook(pitcher_name, batter_name):
     # Example: Simulate running part of the notebook
+    zones = gr.Image("zones.png")
     pitcher = pitcher_name.split()
     batter = batter_name.split()
-    data_pitcher = load_statcast.get_pitcher_data(pitcher[1], pitcher[0])
+
+    try:
+       data_pitcher = load_statcast.get_pitcher_data(pitcher[1], pitcher[0])
+       obs_pitcher = p.get_obs(data_pitcher)
+    except:
+       return "Whoops, looks like that pitcher name was not valid."
+
     arsenal = list(data_pitcher['pitch_type'].drop_duplicates())
-    data_batter = load_statcast.get_batter_data(batter[1], batter[0])
-    obs_pitcher = p.get_obs(data_pitcher)
-    obs_batter = p.get_obs(data_batter)
+
+    try:
+        data_batter = load_statcast.get_batter_data(batter[1], batter[0])
+        obs_batter = p.get_obs(data_batter)
+    except:
+       return "Whoops, looks like that batter name was not valid."
 
     Qp = Q.copy()
     Qp = model.QLearn(Qp, obs_pitcher, 0.3)
@@ -27,8 +37,6 @@ def run_notebook(pitcher_name, batter_name):
     for i in range(len(seq)):
       pitch_sequence += (states[i] + ": "+ str(p.pitches[seq[i][0]]) + ", Zone " + str(seq[i][1]) + "\n")
 
-    zones = gr.Image("zones.png")
-
     return pitch_sequence, zones
 
 # start by initializing Q with all data
@@ -38,8 +46,9 @@ model = QLearning(p)
 Q = model.initialize_q(data)
 
 title = "Pitch Perfect"
-desc = "Welcome to the Pitch Perfect pitch sequence optimizer! Please type in a pitcher and batter's first and last names and Pitch Perfect will solve the pitch sequence. Example: pitcher Chris Sale vs batter Aaron Judge"
+f = open("description.md")
+desc = f.read()
 
-interface = gr.Interface(theme=gr.themes.Soft(), fn=run_notebook, inputs=["text", "text"], outputs=["text", "image"], title=title,
+interface = gr.Interface(theme=gr.themes.Soft(), fn=run_notebook, inputs=[gr.Textbox(label="Pitcher Name"), gr.Textbox(label="Batter Name")], outputs=[gr.Textbox(label="The predicted optimal pitch sequence is:"), "image"], title=title,
                 description=desc)
 interface.launch(debug=True)
---
+++ description.md
@@ -0,0 +1,5 @@
+Welcome to the Pitch Perfect pitch sequence optimizer! Please type in a pitcher and batter's first and last names and Pitch Perfect will solve the pitch sequence. Example: pitcher Chris Sale vs batter Aaron Judge
+
+Looking for a list of pitchers? Try [here](https://baseballsavant.mlb.com/leaderboard/custom?year=2024&type=pitcher&filter=&min=q&selections=pa%2Ck_percent%2Cbb_percent%2Cwoba%2Cxwoba%2Csweet_spot_percent%2Cbarrel_batted_rate%2Chard_hit_percent%2Cavg_best_speed%2Cavg_hyper_speed%2Cwhiff_percent%2Cswing_percent&chart=false&x=pa&y=pa&r=no&chartType=beeswarm&sort=xwoba&sortDir=asc)
+
+Looking for a list of batters? Try [here](https://baseballsavant.mlb.com/leaderboard/custom?year=2024&type=batter&filter=&min=q&selections=pa%2Ck_percent%2Cbb_percent%2Cwoba%2Cxwoba%2Csweet_spot_percent%2Cbarrel_batted_rate%2Chard_hit_percent%2Cavg_best_speed%2Cavg_hyper_speed%2Cwhiff_percent%2Cswing_percent&chart=false&x=pa&y=pa&r=no&chartType=beeswarm&sort=xwoba&sortDir=desc)
